03132ad016dc312041ec10f9c378c7d6425b7b38
thaitoos <tomasz.otreba2@gmail.com>
2022-06-16 21:43:16 +0200

    deleted secret key

--- codeforces_project/codeforces_project/settings.py
+++ codeforces_project/codeforces_project/settings.py
@@ -22,7 +22,8 @@ MEDIA_URL = '/media/'
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+#SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = ""
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
