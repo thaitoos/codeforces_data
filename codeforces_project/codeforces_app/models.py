9e4567fe9a96f6d768d6b58a012449ec4c4df7b9
thaitoos <tomasz.otreba2@gmail.com>
2022-06-17 22:03:35 +0200

    added forms

--- codeforces_project/codeforces_app/models.py
+++ codeforces_project/codeforces_app/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Contestant(models.Model):
     rating = models.IntegerField()
-    handle = models.CharField(max_length=256)
+    handle = models.CharField(max_length=256, unique=True)
+    handle_with_case = models.CharField(max_length=256, unique=True,blank=True, null=True)
     index = models.IntegerField(unique=True, blank = True, null= True)
     def get_top_percent(self):
         for i in range(19):
@@ -12,5 +13,5 @@ class Contestant(models.Model):
         return self.objects.get(index = 2)
 
 class Data(models.Model):
-    name = models.CharField(max_length=1024)
+    name = models.CharField(max_length=1024,unique=True)
     value = models.CharField(max_length=1000000)
--- codeforces_project/codeforces_app/views.py
+++ codeforces_project/codeforces_app/views.py
@@ -1,17 +1,23 @@
+from email.policy import default
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from numpy import average
 import requests
 from codeforces_app.models import Contestant, Data
 from django.contrib.admin.views.decorators import staff_member_required
 import json
+from django import forms
+
+class RatingForm(forms.Form):
+    rating = forms.IntegerField(min_value=-100,max_value=4000, initial=1500)
+class HandleForm(forms.Form):
+    username = forms.CharField(max_length=128, widget=forms.TextInput(attrs={'placeholder': 'Your username'}))
 # Create your views here.
-def index(request):
-    return render(request, "codeforces_app/index.html")
-    #return HttpResponse('yo')
-#@staff_member_required
-def get_contestant_place(rating, size):#returns number of cotestant with a lower or equal rating
+
+def get_contestant_place(rating:int, size: int):#returns number of cotestant with a lower or equal rating
     #if rating < -100 or rating > 4500:
     #    return -1
+    size = int(size)
     l = int(1)
     r = int(size)
     while(l<r):
@@ -27,6 +33,106 @@ def get_contestant_place(rating, size):#returns number of cotestant with a lower
         return l
     return l-1
 
+
+def index(request):
+    if request.method == 'POST':
+        if "submit-handle" in request.POST:
+            form = HandleForm(request.POST)
+            if form.is_valid():
+                handle = form.cleaned_data['username']
+                handle = handle.lower()
+                if not Contestant.objects.filter(handle = handle).exists():
+                    rating_form = RatingForm()
+                    handle_form = HandleForm()
+                    return render(request, "codeforces_app/index.html", {
+                        'rating_form' : rating_form,
+                        'handle_form': handle_form,
+                        'load_type': 'error',
+                        'message': 'invalid username(new or inactive accounts may be unavailable)',
+                    })
+                contestant = Contestant.objects.get(handle = handle)
+                size = int(Data.objects.get(name = 'size').value)
+                average = float(Data.objects.get(name = 'average').value)
+                contestants_worse = get_contestant_place(contestant.rating, size)
+                percent_worse = round(contestants_worse*100/size,2)
+                distance_to_average = abs(contestant.rating - average)
+                distance_to_average = round(distance_to_average,2)# rounding behaves weirdly
+                above = contestant.rating > average
+                rating_form = RatingForm()
+                handle_form = HandleForm()
+                username = contestant.handle_with_case
+                return render(request, "codeforces_app/index.html", {
+                    'rating_form' : rating_form,
+                    'handle_form': handle_form,
+                    'load_type': 'handle',
+                    'percent_worse': percent_worse,
+                    'contestants_worse': contestants_worse,
+                    'distance_to_average': distance_to_average,
+                    'above': above,
+                    'rating': contestant.rating,
+                    'username': username,
+                })
+            else:
+                rating_form = RatingForm()
+                handle_form = HandleForm()
+                return render(request, "codeforces_app/index.html", {
+                    'rating_form' : rating_form,
+                    'handle_form': handle_form,
+                    'load_type': 'error',
+                    'message': 'form wasnt valid',
+                })
+        if 'submit-rating' in request.POST:
+            form = RatingForm(request.POST)
+            if form.is_valid():
+                rating = form.cleaned_data['rating']
+                rating = int(rating)
+                if rating < -200 or rating > 4500:
+                    rating_form = RatingForm()
+                    handle_form = HandleForm()
+                    return render(request, "codeforces_app/index.html", {
+                        'rating_form' : rating_form,
+                        'handle_form': handle_form,
+                        'load_type': 'error',
+                        'message': 'invalid username(new or inactive accounts may be unavailable)',
+                    })
+                size = int(Data.objects.get(name = 'size').value)
+                average = float(Data.objects.get(name = 'average').value)
+                contestants_worse = get_contestant_place(rating, size)
+                percent_worse = round(contestants_worse*100/size,2)
+                distance_to_average = abs(rating - average)
+                distance_to_average = round(distance_to_average,2)# rounding behaves weirdly
+                above = rating > average
+                rating_form = RatingForm()
+                handle_form = HandleForm()
+                return render(request, "codeforces_app/index.html", {
+                    'rating_form' : rating_form,
+                    'handle_form': handle_form,
+                    'load_type': 'rating',
+                    'percent_worse': percent_worse,
+                    'contestants_worse': contestants_worse,
+                    'distance_to_average': distance_to_average,
+                    'above': above,
+                    'rating': rating,
+                })
+            else:
+                rating_form = RatingForm()
+                handle_form = HandleForm()
+                return render(request, "codeforces_app/index.html", {
+                    'rating_form' : rating_form,
+                    'handle_form': handle_form,
+                    'load_type': 'error',
+                    'message': 'form wasnt valid',
+                })    
+
+    rating_form = RatingForm()
+    handle_form = HandleForm()
+    return render(request, "codeforces_app/index.html", {
+        'rating_form' : rating_form,
+        'handle_form': handle_form,
+    })
+
+
+#@staff_member_required
 def get_stats(request):
     data = requests.get('https://codeforces.com/api/user.ratedList')
     if(data.status_code!=200):
@@ -39,7 +145,7 @@ def get_stats(request):
     sum = 0
     ind = 1
     for user in data['result']:
-        contestant_list+=[Contestant(rating = int(user['rating']), handle = user['handle'], index = ind)]
+        contestant_list+=[Contestant(rating = int(user['rating']), handle = user['handle'].lower(),handle_with_case = user['handle'], index = ind)]
         ctr+=1
         sum+=user['rating']
         ind+=1
