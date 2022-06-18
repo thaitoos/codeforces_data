c0215265fa3c8e04e0804c4c09786dac4e890f38
thaitoos <tomasz.otreba2@gmail.com>
2022-06-18 14:43:08 +0200

    added simple graph for rating distrubtion

--- codeforces_project/codeforces_app/static/show.js
+++ codeforces_project/codeforces_app/static/show.js
@@ -1,11 +1,35 @@
 document.addEventListener('DOMContentLoaded', function(){
     const body = document.querySelector('body')
     let json_percentiles = JSON.parse(document.getElementById('json_percentiles').textContent)
-    console.log(document.getElementById('json_percentiles').textContent)
-    for(let key = 1; key<=99; key++){
-        const div = document.createElement('div')
-        div.innerHTML = json_percentiles[key]
-        body.append(div)
+    let average = JSON.parse(document.getElementById('average').textContent)
+    console.log(average)
+    //console.log(document.getElementById('json_percentiles').textContent)
+    labels = [];
+    values = [];
+    colors = [];
+    for(let key = 1; key<=99; key+=2){
+        //const div = document.createElement('div')
+        //div.innerHTML = json_percentiles[key]
+        //body.append(div)
+        labels.push(key)
+        values.push(json_percentiles[key])
+        colors.push('#dd0453');
     }
+    var options = {
+        chart: {
+            type: 'line'
+        },
+        series: [{
+            name: 'rating',
+            data: values
+        }],
+        xaxis: {
+            categories: labels
+        }
+    }
+    // use colored bars?
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
 
+    chart.render();
+    
 })
--- codeforces_project/codeforces_app/views.py
+++ codeforces_project/codeforces_app/views.py
@@ -170,6 +170,7 @@ def show(request):
     json_percentiles = Data.objects.get(name='percentiles').value
     json_percentiles = json.loads(json_percentiles)
     return render(request, 'codeforces_app/show.html', {
-        'json_percentiles' : json_percentiles
+        'json_percentiles' : json_percentiles,
+        'average' : Data.objects.get(name = 'average').value
     })
 
